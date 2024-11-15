dac5f5032cb6702137ed4288ec45530f903a0bbf
dburger47 <Danielburger47@gmail.com>
2024-11-15 10:49:39 -0500

    Update MultChoice_Math.ino

--- MultChoice_Math.ino
+++ MultChoice_Math.ino
@@ -3,6 +3,12 @@
 // The user will need to select the correct expression that matches the given solution.
 // Inputs to the function will be 1. Number of problems, maybe difficulty???
 void createMathProblem(int numProblems) {
+  // create an expression with 3-4 operands that add or subtract to a number between x and y (probably 1ish and 20ish)
+    // use random() to generate the operands, and to determine if + or - between them
+
+  // solve the created problem
+
+  // change one or two of the operands to create a wrong expression
   
 }
 
