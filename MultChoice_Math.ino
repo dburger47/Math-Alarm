3297dbf96c3194b7bbb46fe26bb9179cfcbd9ff6
dburger47 <Danielburger47@gmail.com>
2024-11-15 10:34:46 -0500

    Add files via upload

---
+++ MultChoice_Math.ino
@@ -0,0 +1,17 @@
+
+// This function will create a math problem, consisting of a solution and two expressions.
+// The user will need to select the correct expression that matches the given solution.
+// Inputs to the function will be 1. Number of problems, maybe difficulty???
+void createMathProblem(int numProblems) {
+  
+}
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  
+
+}
